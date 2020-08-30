03d20f182cd9edbe6afc645a255cea1ae1abe4d2
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2020-08-30 16:19:32 +1000

    Migrated UWP to Xamarin.Forms
    Updated Xamarin.Forms for MatrixFormatter

--- MatrixFormatter/MatrixFormatter.UWP/App.xaml.cs
+++ MatrixFormatter/MatrixFormatter.UWP/App.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 namespace MatrixFormatter.UWP
@@ -49,6 +39,8 @@ namespace MatrixFormatter.UWP
                 rootFrame = new Frame();
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
+
+                Xamarin.Forms.Forms.Init(e);
 
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
