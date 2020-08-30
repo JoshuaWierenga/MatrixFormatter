27d90dc52da7cf2ce9938710e02ab04d1cdc8345
Joshua Wierenga <joshua.wierenga.student@gmail.com>
2020-08-31 00:06:15 +1000

    Add Matrix Size Selection

--- MatrixFormatter/MatrixFormatter/MainPage.xaml.cs
+++ MatrixFormatter/MatrixFormatter/MainPage.xaml.cs
@@ -1,18 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace MatrixFormatter
 {
     public partial class MainPage : ContentPage
     {
+        public static readonly BindableProperty MatrixRowsProperty =
+            BindableProperty.Create(
+                propertyName: "MatrixRows",
+                returnType: typeof(string),
+                declaringType: typeof(MainPage),
+                "0",
+                BindingMode.OneWayToSource);
+
+        public static readonly BindableProperty MatrixColumnsProperty =
+            BindableProperty.Create(
+                propertyName: "MatrixColumns",
+                returnType: typeof(string),
+                declaringType: typeof(MainPage),
+                "0",
+                BindingMode.OneWayToSource);
+        public int MatrixRows
+        {
+            get => int.Parse((string)GetValue(MatrixRowsProperty));
+            set => SetValue(MatrixRowsProperty, value);
+        }
+        public int MatrixColumns
+        {
+            get => int.Parse((string)GetValue(MatrixColumnsProperty));
+            set => SetValue(MatrixColumnsProperty, value);
+        }
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void CreateMatrix_OnClicked(object sender, EventArgs e)
+        {
+            int test = MatrixRows;
+            int test2 = MatrixColumns;
         }
     }
 }
